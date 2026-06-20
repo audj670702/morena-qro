1a3bb7c2a5b05bb78a6a84256aa27616353933f9
SCaD <gestor.isp@gmail.com>
2026-06-19 22:26:36 -0600

    v1.10.2.8.8.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
 MORENA QRO Capacitación
 Archivo: js/app.js
-Versión: v1.10.2.8.7
+Versión: v1.10.2.8.8
 Alcance: lógica base de navegación PWA usuario
 */
 
@@ -9,7 +9,7 @@ Alcance: lógica base de navegación PWA usuario
    BLOQUE 01. CONFIGURACIÓN
    ========================================================= */
 
-const APP_VERSION = 'v1.10.2.8.7';
+const APP_VERSION = 'v1.10.2.8.8';
 const MOR_API_USUARIO = 'https://www.scad.mx/_functions/morUsuario';
 const MOR_API_DOCUMENTOS = 'https://www.scad.mx/_functions/morDocumentos';
 const MOR_API_ACTIVIDADES = 'https://www.scad.mx/_functions/morActividades';

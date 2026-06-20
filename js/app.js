905b401348db353a610927805f82cfd710f803f6
SCaD <gestor.isp@gmail.com>
2026-06-19 21:22:59 -0600

    v1.10.2.8.4.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
 MORENA QRO Capacitación
 Archivo: js/app.js
-Versión: v1.10.2.8.3
+Versión: v1.10.2.8.4
 Alcance: lógica base de navegación PWA usuario
 */
 
@@ -9,7 +9,7 @@ Alcance: lógica base de navegación PWA usuario
    BLOQUE 01. CONFIGURACIÓN
    ========================================================= */
 
-const APP_VERSION = 'v1.10.2.8.3';
+const APP_VERSION = 'v1.10.2.8.4';
 const MOR_API_USUARIO = 'https://www.scad.mx/_functions/morUsuario';
 const MOR_API_DOCUMENTOS = 'https://www.scad.mx/_functions/morDocumentos';
 const MOR_API_ACTIVIDADES = 'https://www.scad.mx/_functions/morActividades';
@@ -22,7 +22,7 @@ const MOR_API_CONVERSACION_ABRIR = 'https://www.scad.mx/_functions/morConversaci
 const MOR_API_CONVERSACION_MENSAJES = 'https://www.scad.mx/_functions/morConversacionMensajes';
 const MOR_API_MENSAJE_ENVIAR = 'https://www.scad.mx/_functions/morMensajeEnviar';
 const MOR_PANEL_ADM_URL = 'https://www.scad.mx/mor-panel-adm';
-const APP_LOGO_URL = './assets/Logo_MORENA_Qro.png';
+const APP_LOGO_URL = './assets/Logo_Mor.png';
 const SCAD_LOGO_URL = './assets/icon-192.png';
 const MORENA_FACEBOOK_URL = 'https://www.facebook.com/share/1A7utqCu8i/';
 
